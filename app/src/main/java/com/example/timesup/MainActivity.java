038f9204a77b560b534e3ea20b76c58a224d0ca6
Gaetan <gaetan.villeneuve@etu.unilim.fr>
2019-03-17 12:15:31 +0100

    Fin xml page paramètre

--- app/src/main/java/com/example/timesup/MainActivity.java
+++ app/src/main/java/com/example/timesup/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.timesup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button son;
+    Button vibration;
+
+    Button jouer;
+    Button ajoutMot;
+    Button regles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        son = findViewById(R.id.xml_son);
+        vibration = findViewById(R.id.xml_vibration);
+
+        jouer = findViewById(R.id.xml_jouer);
+        ajoutMot = findViewById(R.id.xml_ajoutMot);
+        regles = findViewById(R.id.xml_regles);
+
+        son.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //active/désactive son
+            }
+        });
+        vibration.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //active/désactive vibration
+            }
+        });
+
+        jouer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ParametrePartie.class);
+                startActivity(intent);
+            }
+        });
+
+        ajoutMot.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AjoutDeMot.class);
+                startActivity(intent);
+            }
+        });
+
+        regles.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ReglesDuJeu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/timesup/ParametrePartie.java
+++ app/src/main/java/com/example/timesup/ParametrePartie.java
@@ -2,12 +2,41 @@ package com.example.timesup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ParametrePartie extends AppCompatActivity {
+    Button moinsEquipe;
+    Button moinsTemps;
+    Button moinsMots;
+    Button plusEquipe;
+    Button plusTemps;
+    Button plusMots;
+
+    TextView nbEquipe;
+    TextView nbTemps;
+    TextView nbMots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametre);
+
+        //Boutons moins
+        moinsEquipe = findViewById(R.id.bouton_moins_equipe_xml);
+        moinsTemps = findViewById(R.id.bouton_moins_equipe_xml);
+        moinsMots = findViewById(R.id.bouton_moins_equipe_xml);
+
+        //Boutons plus
+        plusEquipe = findViewById(R.id.bouton_plus_equipe_xml);
+        plusTemps = findViewById(R.id.bouton_plus_temps_xml);
+        plusMots = findViewById(R.id.bouton_plus_mots_xml);
+
+        //label des paramètres
+        nbEquipe = findViewById(R.id.label_nbEquipe_xml);
+        nbTemps = findViewById(R.id.label_nbTemps_xml);
+        nbMots = findViewById(R.id.label_nbMots_xml);
+
+
     }
 }
